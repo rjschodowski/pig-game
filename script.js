9d9fdc2e8f34aa1e9465749ef5f425253eaa8ff5
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-04-06 20:12:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
-const current1EL = document.getElementById('current--1');
+const current1El = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
@@ -25,7 +25,7 @@ const init = function() {
     score0El.textContent = 0;
     score1El.textContent = 0;
     current0El.textContent = 0;
-    current1EL.textContent = 0;
+    current1El.textContent = 0;
 
     diceEl.classList.add('hidden');
     player0El.classList.remove('player--winner');
